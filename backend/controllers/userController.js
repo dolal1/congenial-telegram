f58d4d8b06d9c7fc40d1e8feaccc5b021227b4c5
David OH <holifeilddavid@gmail.com>
2023-06-11 20:04:23 +0300

    Create users is working, but I got tired

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,33 @@
 import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js'
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register user" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Auth user & get token
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 // connect DB
-connectDB()
+connectDB();
 
 // Instacialise app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use("/api/users", userRoutes);
-app.get('/', (req, res) => res.send('Transform'))
+app.get("/", (req, res) => res.send("Transform"));
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Tranformer on ${port} `))
+app.listen(port, () => console.log(`Tranformer on ${port} `));
